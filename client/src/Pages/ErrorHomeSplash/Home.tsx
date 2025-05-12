db68a13e1071de6fc127c98720f5aeacb1874098
Justin <j.hebenstreit25@gmail.com>
2025-05-11 22:11:22 -0500

    added more files from template projects

---
+++ client/src/Pages/ErrorHomeSplash/Home.tsx
@@ -0,0 +1,33 @@
+import Header from "@/Components/PageComponents/Header";
+import '@/SCSS/PageStyles/Home.scss';
+
+const Home = () => {
+
+    return (
+
+        <>
+
+            <Header text="Code Quest" />
+
+            <div className='siteInfo'>
+
+                <p className='siteInfoContent'>
+                    CodeQuest is a personal reference site for full stack web development. It began as a React.js version of a vanilla HTML project from a web development bootcamp and has since grown into a structured space for long-term learning.
+                </p>
+
+                <p className='siteInfoContent'>
+                    The content is continuously updated as I explore new topics, tools, and techniques across frontend, backend, cloud services, and development workflows. It evolves alongside my skills as both a developer and a learner.
+                </p>
+
+                <p className='siteInfoContent'>
+                    Use the navigation menu above to explore different categories. The site is organized for clarity, speed, and ongoing growth as I continue deepening my understanding of modern web development.
+                </p>
+
+            </div>
+
+        </>
+    );
+
+};
+
+export default Home;
---
+++ client/src/Pages/ErrorHomeSplash/SplashScreen.tsx
@@ -0,0 +1,10 @@
+const SplashScreen: React.FC = () => {
+  return (
+    <div className="SplashScreen">
+      <img src="/Favicon/CodeQuest.webp" alt="CodeQuest Logo" className="logo" />
+      <div className="loader" />
+    </div>
+  );
+};
+
+export default SplashScreen;
