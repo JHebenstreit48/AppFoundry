08ce7c7e8f7f4232e6f7ae079e12e9eb194ce5fd
Justin <j.hebenstreit25@gmail.com>
2025-06-02 16:57:11 -0500

    fixed deployment error due to removed files causing errors

--- src/Navigation/CombinedNav/CombinedNavTopics/frameworksNavCombined.ts
+++ src/Navigation/CombinedNav/CombinedNavTopics/frameworksNavCombined.ts
@@ -2,10 +2,6 @@ import { Page } from '@/Navigation/CombinedNav/CombinedNavAndTypes/NavigationTyp
 
 // ---------------------------------- React Native Navigation Start ----------------------------------
 import RNBasicsNavigation from '@/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Basics';
-import RNStateNavigation from '@/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/State';
-import RNFeaturesNavigation from '@/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Features';
-import RNFormsNavigation from '@/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Forms';
-import RNNavNavigation from '@/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Navigate';
 import RNAdvancedNavigation from '@/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Advanced';
 import RNTestingNavigation from '@/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Testing';
 import RNToolsNavigation from '@/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Tools';
@@ -18,10 +14,6 @@ const frameworksNav: Page = {
       name: 'React Native',
       subpages: [
         RNBasicsNavigation,
-        RNStateNavigation,
-        RNFeaturesNavigation,
-        RNFormsNavigation,
-        RNNavNavigation,
         RNAdvancedNavigation,
         RNTestingNavigation,
         RNToolsNavigation
