4b06e48245b7ecabf7f3df3069347aab79618b3c
Justin <j.hebenstreit25@gmail.com>
2025-06-13 19:39:19 -0500

    updated Core Concepts Navigation regarding Props and State

---
+++ src/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Basics/Sections/CoreConcepts/PropsAndState.ts
@@ -0,0 +1,17 @@
+import { Subpage } from '@/Navigation/CombinedNav/CombinedNavAndTypes/NavigationTypes';
+
+const RNPropsStateNav: Subpage = {
+  name: 'Props & State',
+  subpages: [
+    {
+      name: 'Props',
+      path: '/reactnative/basics/core/propsstate/props'
+    },
+    {
+      name: 'State',
+      path: '/reactnative/basics/core/propsstate/state'
+    }
+  ]
+};
+
+export default RNPropsStateNav;
--- src/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Basics/Sections/CoreConcepts/index.ts
+++ src/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Basics/Sections/CoreConcepts/index.ts
@@ -2,12 +2,14 @@ import { Subpage } from '@/Navigation/CombinedNav/CombinedNavAndTypes/Navigation
 import RNCoreConceptsMainNav from '@/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Basics/Sections/CoreConcepts/Main';
 import RNComponentsNav from '@/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Basics/Sections/CoreConcepts/Components';
 import RNStylingNav from '@/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Basics/Sections/CoreConcepts/Styling';
+import RNPropsStateNav from '@/Navigation/IndividualNav/FrameworksNavigation/ReactNativeNavigation/Basics/Sections/CoreConcepts/PropsAndState';
 
 const RNCoreConceptsNav: Subpage = {
   name: 'Core Concepts',
   subpages: [
     ...RNCoreConceptsMainNav,
     RNComponentsNav,
+    RNPropsStateNav,
     RNStylingNav
   ]
 };
