adbf09a53292fee96bccbf754afe2d13099daf2a
Justin <j.hebenstreit25@gmail.com>
2025-10-25 00:51:17 -0500

    made project match similar projects.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '@/App';
 import ErrorPage from '@/Pages/Special/Error';
 import Home from '@/Pages/Special/Home';
+import About from '@/Pages/Special/About';
 
 import frameworks from '@/routes/FullSections/frameworks';
 import enginesRoutes from '@/routes/FullSections/engines';
@@ -19,6 +20,7 @@ export const router = createBrowserRouter([
         index: true,
         element: <Home />
       },
+      { path: 'about', element: <About /> },
       ...frameworks,
       ...enginesRoutes,
       ...languagesRoutes,
