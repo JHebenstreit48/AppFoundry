4876354eaa44c55a6c9ee77126ed2ff973955a9d
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-02-23 16:46:37 -0600

    added new Glossary tab

--- src/domain/navigation/mainTabs.ts
+++ src/domain/navigation/mainTabs.ts
@@ -4,12 +4,14 @@ import languages from '@/Navigation/Combined/Topics/languages';
 import frameworks from '@/Navigation/Combined/Topics/frameworks';
 import tools from '@/Navigation/Combined/Topics/tools';
 import resources from '@/Navigation/Combined/Topics/resources';
+import glossary from '@/Navigation/Combined/Topics/glossary';
 
 const pages: Subpage[] = [
   languages,
   frameworks,
   tools,
-  resources
+  resources,
+  glossary
 ];
 
 export default pages;
