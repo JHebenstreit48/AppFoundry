b8ab7476bfb187e2cb93eb1288204f5036cdd2c9
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-02-26 14:17:21 -0600

    updated several files

--- src/domain/navigation/mainTabs.ts
+++ src/domain/navigation/mainTabs.ts
@@ -1,10 +1,10 @@
 import type { Subpage } from '@/types/navigation';
 
-import languages from '@/Navigation/Combined/Topics/languages';
-import frameworks from '@/Navigation/Combined/Topics/frameworks';
-import tools from '@/Navigation/Combined/Topics/tools';
-import resources from '@/Navigation/Combined/Topics/resources';
-import glossary from '@/Navigation/Combined/Topics/glossary';
+import languages from '@/Navigation/Individual/Combined/Topics/languages';
+import frameworks from '@/Navigation/Individual/Combined/Topics/frameworks';
+import tools from '@/Navigation/Individual/Combined/Topics/tools';
+import resources from '@/Navigation/Individual/Combined/Topics/resources';
+import glossary from '@/Navigation/Individual/Combined/Topics/glossary';
 
 const pages: Subpage[] = [
   languages,
