24c18888c60deabd43b9ff2fa328f4eee25b1cb0
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-07-30 11:41:56 -0500

    rename: temp step for Button.tsx -> button.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 import Footer from '@/components/shared/Footer';
 import RouteTracker from '@/components/shared/analytics/RouteTracker';
-import BackToTopGate from '@/components/shared/BackToTop/Gate';
+import BackToTopGate from '@/components/shared/backToTop/Gate';
 
 import '@/scss/Page/Page.scss';
 import '@/scss/Shared/Header/index.scss';
